a0ed5da3187a3fa0e127cdbbd6e043ae7739ae3f
Marek Beran <bercek71@gmail.com>
2024-10-23 15:19:57 +0200

    lab01 showing all functions

--- Lab01/main.py
+++ Lab01/main.py
@@ -3,11 +3,29 @@ from BlindSearch import BlindSearch
 from animation import run_animation
 
 def main():
+    function = Function(FunctionType.ACKLEY)
+    # blind_search = BlindSearch(function, iterations=50)
+    # blind_search.search_min()
+    # blind_search.visualize_solution()
+    # run_animation(blind_search.history, function)
+
+    function.show_plot()
+    function = Function(FunctionType.SPHERE)
+    function.show_plot()
     function = Function(FunctionType.GRIEWANK)
-    blind_search = BlindSearch(function, iterations=50)
-    blind_search.search_min()
-    blind_search.visualize_solution()
-    run_animation(blind_search.history, function)
+    function.show_plot()
+    function = Function(FunctionType.ROSENBROCK)
+    function.show_plot()
+    function = Function(FunctionType.LEVY)
+    function.show_plot()
+    function = Function(FunctionType.ZAKHAROV)
+    function.show_plot()
+    function = Function(FunctionType.RASTRIGIN)
+    function.show_plot()
+    function = Function(FunctionType.MICHALEWICZ)
+    function.show_plot()
+    function = Function(FunctionType.SCHWEFEL)
+    function.show_plot()
 
 
 if __name__ == '__main__':
