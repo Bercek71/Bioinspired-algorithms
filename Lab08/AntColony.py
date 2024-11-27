328ad27336c323d438d6f002362471a01759a838
Marek Beran <bercek71@gmail.com>
2024-11-27 12:05:29 +0100

    update

--- Lab08/AntColony.py
+++ Lab08/AntColony.py
@@ -18,7 +18,6 @@ class AntColony:
             for i in range(num_cities)
         ]
 
-
     def solve(self, tsp, num_ants, num_generations):
         self.history = []
         self.initialize_pheromones_and_distances(tsp)
