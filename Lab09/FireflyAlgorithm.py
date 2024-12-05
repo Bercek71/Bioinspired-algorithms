a7d78feebb759fef60f98d9bc196d3d6a4056ff3
Marek Beran <bercek71@gmail.com>
2024-12-05 10:07:39 +0100

    lab09

---
+++ Lab09/FireflyAlgorithm.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+class FireflyAlgorithm:
+    def __init__(self, population_size, function, m_max, t, dimension=2, alpha=0.5, beta_0=1.0, gamma=1.0):
+        self.population_size = population_size
+        self.function = function
+        self.m_max = m_max
+        self.dimension = dimension
+        self.t = t
+        self.alpha = alpha  # Randomness parameter
+        self.beta_0 = beta_0  # Initial attractiveness
+        self.gamma = gamma  # Absorption coefficient
+        self.population = np.random.uniform(
+            self.function.lower_bound, self.function.upper_bound, (population_size, self.dimension)
+        )
+        self.best_position = None
+        self.best_fitness = float('inf')
+        self.history = []  # Historie iterací pro animaci
+
+    def move_fireflies(self, i, j):
+        # Vzdálenost mezi světluškami
+        r = np.linalg.norm(self.population[i] - self.population[j])
+        # Přitažlivost
+        beta = self.beta_0 * np.exp(-self.gamma * r**2)
+        # Pohyb světlušky
+        self.population[i] += beta * (self.population[j] - self.population[i]) + \
+                              self.alpha * (np.random.rand(self.dimension) - 0.5)
+        # Omezení hodnot na povolený rozsah
+        self.population[i] = np.clip(self.population[i], self.function.lower_bound, self.function.upper_bound)
+
+    def evaluate_population(self):
+        # Výpočet fitness hodnot pro všechny jedince v populaci
+        fitness_values = np.array([self.function.evaluate(ind) for ind in self.population])
+        return fitness_values
+
+    def search_min(self):
+        for gen in range(self.m_max):
+            fitness_values = self.evaluate_population()
+            # Aktualizace nejlepší pozice
+            best_index = np.argmin(fitness_values)
+            if fitness_values[best_index] < self.best_fitness:
+                self.best_fitness = fitness_values[best_index]
+                self.best_position = self.population[best_index].copy()
+            self.history.append((self.population[best_index].copy(), self.best_fitness))
+            for i in range(self.population_size):
+                for j in range(self.population_size):
+                    if fitness_values[j] < fitness_values[i]:
+                        self.move_fireflies(i, j)
+        return self.best_position, self.best_fitness
+
+    def get_best_solution(self):
+        return self.best_position, self.best_fitness
---
+++ Lab09/main.py
@@ -0,0 +1,19 @@
+from Functions import Function, FunctionType
+from animation import  run_animation
+from FireflyAlgorithm import FireflyAlgorithm
+
+def main():
+
+    function = Function(FunctionType.ACKLEY)
+
+    fireFly = FireflyAlgorithm(population_size=20, function=function, m_max=100, t=0.1, dimension=2, alpha=0.5, beta_0=1.0, gamma=1.0)
+
+    best_solution, best_fitness = fireFly.search_min()
+
+    function.show_plot(custom_point=best_solution)
+    run_animation(fireFly.history, function)
+
+
+
+if __name__ == '__main__':
+    main()
